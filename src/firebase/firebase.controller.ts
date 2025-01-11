11366c1f6068d4d35f83d96baecf35c68f25b302
KleberMera <klebermera2016@gmail.com>
2025-01-11 11:44:16 -0500

    firebase controller

--- src/firebase/firebase.controller.ts
+++ src/firebase/firebase.controller.ts
@@ -1,9 +1,34 @@
-import { Controller } from "@nestjs/common";
-import { FirebaseService } from "./firebase.service";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { FirebaseService } from './firebase.service';
 
 @Controller('firebase')
 export class FirebaseController {
   constructor(private readonly firebaseService: FirebaseService) {}
 
- 
+  // Nuevos endpoints para Google
+  @HttpCode(HttpStatus.OK)
+  @Post('google/login')
+  async googleLogin(@Body() googleData: { token: string }) {
+    return this.firebaseService.loginWithGoogle(googleData);
+  }
+
+  // Opcional: endpoint para validar token de Google
+  @Get('google/verify')
+  //@UseGuards(FirebaseAuthGuard) // Asegúrate de crear este guard
+  async verifyGoogleToken(@Req() req) {
+    return {
+      message: 'Token válido',
+      user: req.user,
+    };
+  }
+
+  
 }
