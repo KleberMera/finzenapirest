b495d34156d4b68022b53b40e0aec425120aa680
KleberMera <klebermera2016@gmail.com>
2025-03-15 13:11:35 -0500

    Inicializar Firebase Admin en el arranque de la aplicación y simplificar la configuración del servidor

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,22 @@ import { serviceAccount } from './libs/services-account';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Initialize Firebase Admin
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+  });
   // Enable CORS
   app.enableCors({
     origin: ['http://localhost:4200', 'https://fin-zen.vercel.app', 'http://127.0.0.1:8080'],
   });
 
-  // Initialize Firebase Admin
-  firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
-  });
 
-  const port = process.env.PORT || 3000;
-  app.setGlobalPrefix('api');
-  await app.listen(port);
-  console.log(`Server is running on port ${port}`);
+
+ // const port = process.env.PORT || 3000;
+  //app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT ?? 3000);
+
+ // console.log(`Server is running on port ${port}`);
 }
 
 bootstrap();
