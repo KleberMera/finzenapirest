0475d0dd9648344f09ade4c6118e5af37dd42216
KleberMera <klebermera2016@gmail.com>
2025-04-06 01:09:52 -0500

    Agregar guardia de autenticación con verificación de rutas públicas utilizando Reflector y metadatos.

--- src/guards/token.guard.ts
+++ src/guards/token.guard.ts
@@ -2,16 +2,33 @@ import {
   CanActivate,
   ExecutionContext,
   Injectable,
+  SetMetadata,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
 @Injectable()
 export class TokenGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
@@ -23,6 +40,7 @@ export class TokenGuard implements CanActivate {
         secret: process.env.JWT_SECRET,
       });
       console.log('Payload:', payload); // Log the payload for debugging
+
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
       throw new UnauthorizedException();
